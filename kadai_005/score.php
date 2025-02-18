5afb7bfcc5eff6844cf0ccadb61c988fd03c0d79
yosuke19930913 <yosuke19930913@gmail.com>
2025-02-18 16:25:19 +0900

    修正

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -1,4 +1,35 @@
-<?php
-        $number = 80+60+55+40+100+25+80+95+30+60;
+<!DOCTYPE html>
+<html lang="ja">
 
-        echo $number / 10;
+<head>
+   <meta charset="UTF-8">
+   <title>KADAI_005</title>
+</head>
+
+<body>
+   <p>
+       <?php
+      $score1 = 80;
+      $score2 = 60;
+      $score3 = 55;
+      $score4 = 40;
+      $score5 = 100;
+      $score6 = 25;
+      $score7 = 80;
+      $score8 = 95;
+      $score9 = 30;
+      $score10 = 60;
+
+      $total_score = $score1 + $score2 + $score3 +$score4 + $score5 + $score6 +$score7 + $score8 + $score9 + $score10;
+
+      echo "合計点: " . $total." <br> " ;
+
+      $average = $total / 10;
+
+      echo "平均点: " . $average;
+
+       ?>
+   </p>
+
+
+   
