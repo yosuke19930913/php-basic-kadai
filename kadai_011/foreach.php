431cbcf2b73884828ac62b4b690224749e3ca20c
yosuke19930913 <yosuke19930913@gmail.com>
2025-02-21 12:48:22 +0900

    kadai_011

---
+++ kadai_011/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>kadai_011</title>
+</head>
+
+<body>
+<p>
+        <?php
+        $personal_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+
+
+      // 連想配列$personal_dataのキーと値を1つずつ順番に出力する
+foreach ($personal_data as $key => $value) {
+  echo "{$key}:{$value} <br>";
+}
+ ?>
+    </p>
+</body>
+
+</html>
